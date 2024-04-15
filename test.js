8df29dd5e4f936d61891f06b6e6ecd2f0cda4e99
omar <oh1149725@gmail.com>
2024-04-15 15:50:05 +0200

    new

---
+++ test.js
@@ -0,0 +1,20 @@
+
+
+
+
+let x=document.getElementById("is_admin")
+
+
+
+
+if(x.checked){
+
+
+    document.getElementById("sign_bt").href="index_admin.html"
+}
+
+
+else{
+
+    document.getElementById("sign_bt").href="index_user.html"
+}
